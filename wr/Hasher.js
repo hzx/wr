a512bbcd15d0c831f67ec81d84cd265149184ee5
Dmitry Dementev <dem@oldschooldev.com>
2014-12-10 17:12:52 +0300

    fix bug: Hasher.generate

--- wr/Hasher.js
+++ wr/Hasher.js
@@ -1,26 +1,21 @@
 
 wr.Hasher = function() {
-  var last = null;
-  var increment = 0;
+  this.last = null;
+  this.increment = 0;
 };
+
+wr.Hasher.MAX_NUM = 1000000;
 
 
 wr.Hasher.prototype.generate = function() {
-  var hash = (new Date()).getTime().toString(16);
-
-  if (hash === this.last) {
-    ++this.increment;
-    hash += "-" + this.increment.toString(16);
-  } else {
-    this.increment = 0;
-  }
-
-  this.last = hash;
-  return hash;
+  this.increment =
+    this.increment > wr.Hasher.MAX_NUM ? 0 : this.increment + 1;
+  return (new Date()).getTime().toString(16) + "-" +
+    this.increment.toString(16);
 };
 
-wr.hasher = new wr.Hasher();
 
+wr.hasher = new wr.Hasher();
 wr.hash = function() {
   return wr.hasher.generate();
 };
